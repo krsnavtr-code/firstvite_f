5cd6ffa20520ba3f562db009ac3e066de9bfff39
Krishna Avtar <krishnaavtar955@gmail.com>
2025-07-12 16:07:59 +0530

    module

--- src/components/admin/courses/CourseForm.jsx
+++ src/components/admin/courses/CourseForm.jsx
@@ -1096,7 +1096,7 @@ const CourseForm = ({ isEdit = false }) => {
               }
               className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
             >
-              + Add Week
+              + Add Module
             </button>
           </div>
 
