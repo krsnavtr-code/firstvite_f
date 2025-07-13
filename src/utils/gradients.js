3f132206e2ef5603b7b1f824b10fc2d3d4bea4ba
Krishna Avtar <krishnaavtar955@gmail.com>
2025-07-13 16:58:48 +0530

    color change

--- src/utils/gradients.js
+++ src/utils/gradients.js
@@ -31,16 +31,7 @@ export const getCardBgColor = (item, index = 0) => {
   const hash = Array.from(id).reduce((acc, char) => acc + char.charCodeAt(0), 0);
 
   const bgColors = [
-    'bg-[#F47C26] dark:bg-[#F47C26]/30',
-    // 'bg-green-300 dark:bg-green-900/30',
-    // 'bg-purple-300 dark:bg-purple-900/30',
-    // 'bg-amber-300 dark:bg-amber-900/30',
-    // 'bg-pink-300 dark:bg-pink-900/30',
-    // 'bg-indigo-300 dark:bg-indigo-900/30',
-    // 'bg-teal-300 dark:bg-teal-900/30',
-    // 'bg-rose-300 dark:bg-rose-900/30',
-    // 'bg-emerald-300 dark:bg-emerald-900/30',
-    // 'bg-cyan-300 dark:bg-cyan-900/30',
+    'bg-[#F47C26]/30 dark:bg-[#2563EB]/30',
   ];
 
   return bgColors[Math.abs(hash) % bgColors.length];
