d134b5034ce60ed310c8207a3c9da5f0ea6f7319
Krishna Avtar <113005173+krsnavtr-code@users.noreply.github.com>
2025-07-19 17:48:11 +0530

    Update vite.config.js add a render backend

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4002',
+        target: 'https://firstvite-b.onrender.com',
         changeOrigin: true,
         secure: false,
         ws: true,
